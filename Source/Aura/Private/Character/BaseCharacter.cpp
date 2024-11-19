7af6be3e655f3a1e2ef27c47c3cafa491e3da72b
hayoonleeMe <htdobad@gmail.com>
2024-11-19 21:01:40 +0900

    캐릭터의 기반이 될 BaseCharacter 클래스 생성 #1

---
+++ Source/Aura/Private/Character/BaseCharacter.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/BaseCharacter.h"
+
+ABaseCharacter::ABaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
