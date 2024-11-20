5767a52451a04d966bae187ba5c79c1f0c3730e6
hayoonleeMe <htdobad@gmail.com>
2024-11-20 22:05:16 +0900

    Spell을 등록할 Input Tag 추가

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -14,6 +14,16 @@ public:
 	static FAuraGameplayTags& Get() { return GameplayTags; }
 	static void InitializeNativeGameplayTags();
 
+	/*
+	 *	Input
+	 */
+	FGameplayTag InputTag_LMB;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_1;
+	FGameplayTag InputTag_2;
+	FGameplayTag InputTag_3;
+	FGameplayTag InputTag_4;
+	
 private:
 	static FAuraGameplayTags GameplayTags;
 };
