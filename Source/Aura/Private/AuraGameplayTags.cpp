e116e9469d6de3c8156536c7f3cf66caa517a250
hayoonleeMe <htdobad@gmail.com>
2024-11-23 17:54:29 +0900

    각 Attribute를 나타내는 GameplayTag 추가

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -18,4 +18,30 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_2 = Manager.AddNativeGameplayTag(TEXT("InputTag.2"), TEXT("Input Tag for 2 key"));
 	GameplayTags.InputTag_3 = Manager.AddNativeGameplayTag(TEXT("InputTag.3"), TEXT("Input Tag for 3 key"));
 	GameplayTags.InputTag_4 = Manager.AddNativeGameplayTag(TEXT("InputTag.4"), TEXT("Input Tag for 4 key"));
+
+	/* Primary Attributes */
+	GameplayTags.Attributes_Primary_Strength = Manager.AddNativeGameplayTag(TEXT("Attributes.Primary.Strength"), TEXT("Increases physical damage"));
+	GameplayTags.Attributes_Primary_Intelligence = Manager.AddNativeGameplayTag(TEXT("Attributes.Primary.Intelligence"), TEXT("Increases magical damage"));
+	GameplayTags.Attributes_Primary_Resilience = Manager.AddNativeGameplayTag(TEXT("Attributes.Primary.Resilience"), TEXT("Increases Armor and Armor Penetration"));
+	GameplayTags.Attributes_Primary_Vigor = Manager.AddNativeGameplayTag(TEXT("Attributes.Primary.Vigor"), TEXT("Increases Health"));
+	
+	/* Secondary Attributes */
+	GameplayTags.Attributes_Secondary_Armor = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.Armor"), TEXT("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_ArmorPenetration = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.ArmorPenetration"), TEXT("Ignored Percentage of enemy Armor, increases Critical Hit Chance"));
+	GameplayTags.Attributes_Secondary_BlockChance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.BlockChance"), TEXT("Chance to cut incoming damage in half"));
+	GameplayTags.Attributes_Secondary_CriticalHitChance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.CriticalHitChance"), TEXT("Chance to double damage plus critical hit bonus"));
+	GameplayTags.Attributes_Secondary_CriticalHitDamage = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.CriticalHitDamage"), TEXT("Bonus damage added when a critical hit is scored"));
+	GameplayTags.Attributes_Secondary_CriticalHitResistance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.CriticalHitResistance"), TEXT("Reduces Critical Hit Chance of attacking enemies"));
+	GameplayTags.Attributes_Secondary_HealthRegeneration = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.HealthRegeneration"), TEXT("Amount of Health regenerated every 1 second"));
+	GameplayTags.Attributes_Secondary_ManaRegeneration = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.ManaRegeneration"), TEXT("Amount of Mana regenerated every 1 second"));
+	GameplayTags.Attributes_Secondary_MaxHealth = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.MaxHealth"), TEXT("Maximum amount of Health obtainable"));
+	GameplayTags.Attributes_Secondary_MaxMana = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.MaxMana"), TEXT("Maximum amount of Mana obtainable"));
+	GameplayTags.Attributes_Secondary_FireResistance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.FireResistance"), TEXT("Resistance to Fire Damage"));
+	GameplayTags.Attributes_Secondary_LightningResistance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.LightningResistance"), TEXT("Resistance to Lightning Damage"));
+	GameplayTags.Attributes_Secondary_ArcaneResistance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.ArcaneResistance"), TEXT("Resistance to Arcane Damage"));
+	GameplayTags.Attributes_Secondary_PhysicalResistance = Manager.AddNativeGameplayTag(TEXT("Attributes.Secondary.PhysicalResistance"), TEXT("Resistance to Physical Damage"));
+	
+	/* Vital Attributes */
+	GameplayTags.Attributes_Vital_Health = Manager.AddNativeGameplayTag(TEXT("Attributes.Vital.Health"), TEXT("Amount of damage a player can take before death"));
+	GameplayTags.Attributes_Vital_Mana = Manager.AddNativeGameplayTag(TEXT("Attributes.Vital.Mana"), TEXT("Amount of energy a player can use to cast spells"));
 }
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -23,6 +23,38 @@ public:
 	FGameplayTag InputTag_2;
 	FGameplayTag InputTag_3;
 	FGameplayTag InputTag_4;
+
+	/*
+	 *	Primary Attributes
+	 */
+	FGameplayTag Attributes_Primary_Strength;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Vigor;
+
+	/*
+	 *	Secondary Attributes
+	 */
+	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+	FGameplayTag Attributes_Secondary_FireResistance;
+	FGameplayTag Attributes_Secondary_LightningResistance;
+	FGameplayTag Attributes_Secondary_ArcaneResistance;
+	FGameplayTag Attributes_Secondary_PhysicalResistance;
+
+	/*
+	 *	Vital Attributes
+	 */
+	FGameplayTag Attributes_Vital_Health;
+	FGameplayTag Attributes_Vital_Mana;
 	
 private:
 	static FAuraGameplayTags GameplayTags;
