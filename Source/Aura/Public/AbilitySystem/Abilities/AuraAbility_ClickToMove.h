1e066b740dbb8d81de5bad667fe446945737bc89
hayoonleeMe <htdobad@gmail.com>
2024-11-26 00:25:32 +0900

    마우스 커서의 위치까지 장애물을 피한 Navigation Path를 구하고 AbilityTask를 이용해 매 프레임 캐릭터를 목적지까지 이동시키는 Ability 구현

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraAbility_ClickToMove.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraAbility_ClickToMove.generated.h"
+
+class UNiagaraSystem;
+class UAbilityTask_ClickToMove;
+class AAuraPlayerController;
+class USplineComponent;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbility_ClickToMove : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UAuraAbility_ClickToMove();
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+	bool MoveToDestination() const;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<ACharacter> Character;
+	
+	UPROPERTY()
+	TObjectPtr<AAuraPlayerController> AuraPlayerController;
+	
+	UPROPERTY()
+	TObjectPtr<USplineComponent> SplineComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_ClickToMove> AbilityTask_ClickToMove;
+
+	FVector Destination;
+
+	// 캐릭터가 계속 목적지로 이동해야 하는지를 나타내는 변수
+	uint8 bShouldMove : 1;
+	
+	// 목적지 도착 인정 범위 (제곱값)
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	float SquaredArriveAcceptanceRadius;
+
+	/*
+	 * Effect
+	 */
+	UPROPERTY(EditDefaultsOnly, Category="Aura|Effect")
+	TObjectPtr<UNiagaraSystem> CursorEffect;
+
+	// true일 때 커서 효과 생성
+	uint8 bShouldSpawnCursorEffect : 1;
+};
