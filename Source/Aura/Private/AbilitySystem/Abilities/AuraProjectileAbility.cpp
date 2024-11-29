1887d4c0f58589565ffde758a221ba452c584d3c
hayoonleeMe <htdobad@gmail.com>
2024-11-29 15:22:26 +0900

    Projectile을 발사하는 Ability Class 추가 및 하나의 Projectile을 발사하는 기능 구현

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileAbility.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/AuraProjectileAbility.h"
+
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
+
+UAuraProjectileAbility::UAuraProjectileAbility()
+{
+	NumProjectiles = 1;
+}
+
+void UAuraProjectileAbility::SpawnProjectile(const FVector& TargetLocation)
+{
+	const AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	if (!IsValid(AvatarActor) || !AvatarActor->HasAuthority())	// Only Spawn in Server
+	{
+		return;
+	}
+	check(ProjectileClass);
+
+	// CombatSocket에서 Projectile 발사
+	const FVector CombatSocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(AvatarActor);
+	const FRotator Rotation = (TargetLocation - CombatSocketLocation).Rotation();
+	
+	FTransform SpawnTransform;
+	SpawnTransform.SetLocation(CombatSocketLocation);
+	SpawnTransform.SetRotation(Rotation.Quaternion());
+
+	AActor* OwningActor = GetOwningActorFromActorInfo();
+	if (AAuraProjectile* AuraProjectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(ProjectileClass, SpawnTransform, OwningActor, Cast<APawn>(OwningActor), ESpawnActorCollisionHandlingMethod::AlwaysSpawn))
+	{
+		// Projectile로 데미지를 입히기 위해 설정
+		MakeDamageEffectParams(AuraProjectile->DamageEffectParams, nullptr);
+		AuraProjectile->FinishSpawning(SpawnTransform);
+	}
+}
