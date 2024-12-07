ff6fdd83678793549ad03762c4a23d8310b2561a
hayoonleeMe <htdobad@gmail.com>
2024-12-07 20:16:41 +0900

    Enemy의 원거리 공격 Ability Class 생성 및 Blueprint 생성 Enemy 원거리 공격 Damage Curve 추가

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraAbility_EnemyRange.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/AuraAbility_EnemyRange.h"
+
+#include "AuraGameplayTags.h"
+#include "Interaction/CombatInterface.h"
+
+UAuraAbility_EnemyRange::UAuraAbility_EnemyRange()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UAuraAbility_EnemyRange::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                              const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	// Activated only in server by AI (BTTask)
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	const ICombatInterface* CombatInterface = Cast<ICombatInterface>(AvatarActor);
+	if (!IsValid(AvatarActor) || !CombatInterface)
+	{
+		return;
+	}
+
+	// Caching
+	const FTaggedCombatInfo TaggedCombatInfo = ICombatInterface::Execute_GetTaggedCombatInfo(AvatarActor, FAuraGameplayTags::Get().Abilities_EnemyAttack);
+	check(TaggedCombatInfo.AnimMontage);
+	CachedCombatSocketName = TaggedCombatInfo.CombatSocketName;
+
+	// for Anim Montage Motion Warping
+	// for Spawn Projectile
+	const TWeakObjectPtr<AActor> CombatTargetWeakPtr = CombatInterface->GetCombatTarget();
+	if (CombatTargetWeakPtr.IsValid())
+	{
+		CachedTargetLocation = CombatTargetWeakPtr.Get()->GetActorLocation(); 
+		ICombatInterface::Execute_SetFacingTarget(AvatarActor, CachedTargetLocation);
+	}
+
+	PlayAttackMontage(TaggedCombatInfo.AnimMontage, false);
+	WaitGameplayEvent(FAuraGameplayTags::Get().Event_Montage_EnemyAttack);
+}
+
+void UAuraAbility_EnemyRange::OnEventReceived(FGameplayEventData Payload)
+{
+	const AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	if (!IsValid(AvatarActor) || !AvatarActor->Implements<UCombatInterface>())
+	{
+		return;
+	}
+
+	const FVector& CombatSocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(AvatarActor, CachedCombatSocketName);
+	SpawnProjectile(CachedTargetLocation, CombatSocketLocation);
+
+	FinishAttack();
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraAbility_EnemyRange.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraProjectileAbility.h"
+#include "AuraAbility_EnemyRange.generated.h"
+
+/**
+ * Enemy의 원거리 공격 Ability
+ */
+UCLASS()
+class AURA_API UAuraAbility_EnemyRange : public UAuraProjectileAbility
+{
+	GENERATED_BODY()
+
+public:
+	UAuraAbility_EnemyRange();
+	
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	virtual void OnEventReceived(FGameplayEventData Payload) override;
+
+private:
+	FVector CachedTargetLocation;
+	FName CachedCombatSocketName;
+};
