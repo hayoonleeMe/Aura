3611d3277b07ef19d758d3dd1a8544ac4495e1e1
hayoonleeMe <htdobad@gmail.com>
2024-12-08 21:12:58 +0900

    Combatinterface Function 설명 주석 추가

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -40,25 +40,39 @@ class AURA_API ICombatInterface
 	GENERATED_BODY()
 
 public:
+	// Character Level을 반환
 	virtual int32 GetCharacterLevel() { return 0; }
 
+	// Character의 RoleTag 반환
+	// Role.Player, Role.Enemy
 	virtual FGameplayTag GetRoleTag() const { return FGameplayTag(); }
 
+	// Anim Montage의 MotionWarping을 위한 FacingTarget 설정
 	virtual void SetFacingTarget(const FVector& TargetLocation) {}
 
+	// Character가 죽을 때 호출
 	virtual void Die() {}
 
+	// Character가 죽었는지 반환
 	virtual bool IsDead() const { return false; }
-	
+
+	// Enemy의 AttackRangeRadius, AttackRangeHalfHeight 반환
 	virtual void GetAttackCheckRange(float& OutRadius, float& OutHalfHeight) const {}
-	
+
+	// Character의 InTag에 해당하는 TaggedCombatInfo 반환
 	virtual FTaggedCombatInfo GetTaggedCombatInfo(const FGameplayTag& InTag) const { return FTaggedCombatInfo(); }
 
+	// Character의 Mesh, WeaponMesh에서 이름이 CombatSocketName인 Socket을 찾아 Location 반환
+	// Mesh, WeaponMesh에 Socket이 없으면 ZeroVector 반환
 	virtual FVector GetCombatSocketLocation(const FName& CombatSocketName) const { return FVector::ZeroVector; }
-	
+
+	// Character의 Mesh, WeaponMesh에서 이름이 CombatSocketName인 Socket을 찾아 Transform 반환
+	// Mesh, WeaponMesh에 Socket이 없으면 Identity 반환
 	virtual FTransform GetCombatSocketTransform(const FName& CombatSocketName) const { return FTransform::Identity; }
-	
+
+	// AI Enemy의 공격 대상 저장
 	virtual void SetCombatTarget(AActor* InCombatTarget) {}
 
+	// AI Enemy의 공격 대상을 반환
 	virtual TWeakObjectPtr<AActor> GetCombatTarget() const { return nullptr; }
 };
