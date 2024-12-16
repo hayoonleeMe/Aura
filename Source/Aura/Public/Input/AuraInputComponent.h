acd7c308ccfa6cd0a6c2a4e025ad0f85a9c3e44b
hayoonleeMe <htdobad@gmail.com>
2024-12-16 15:47:47 +0900

    Move AuraInputConfig Class to Data Folder

--- Source/Aura/Public/Input/AuraInputComponent.h
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AuraInputConfig.h"
+#include "Data/AuraInputConfig.h"
 #include "EnhancedInputComponent.h"
 #include "AuraInputComponent.generated.h"
 
