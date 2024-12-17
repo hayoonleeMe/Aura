aadb92adbb51173ba54e662adb10e46753e12816
hayoonleeMe <htdobad@gmail.com>
2024-12-17 18:11:18 +0900

    새로운 ClickToMove Trace Channel 생성

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -12,6 +12,10 @@
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_EQS_Overlap ECollisionChannel::ECC_GameTraceChannel2
 
+// Player의 ClickToMove Ability에서 마우스 커서 위치를 구할 때 사용할 Trace Channel
+// Default Response Block, Ignore BaseCharacter's Mesh and Capsule 
+#define ECC_ClickToMove ECollisionChannel::ECC_GameTraceChannel3
+
 // ============================================================================
 // Custom Log Macro
 // ============================================================================
