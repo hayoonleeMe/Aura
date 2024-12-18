5a4948dc066bbebf075b4950d26201b0a04e0983
hayoonleeMe <htdobad@gmail.com>
2024-12-18 14:26:09 +0900

    Spell Menu에서 사용할 SpellMenuWidgetController Class 생성 Blueprint 생성

---
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraWidgetController.h"
+#include "SpellMenuWidgetController.generated.h"
+
+/**
+ * Spell Menu에서 사용할 Widget Controller
+ */
+UCLASS()
+class AURA_API USpellMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+
+};
