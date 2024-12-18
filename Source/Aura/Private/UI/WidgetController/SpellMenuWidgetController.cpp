58303dd89e971369c86609ffb78677fd433bde2f
hayoonleeMe <htdobad@gmail.com>
2024-12-18 15:14:36 +0900

    SpellMenuWidgetController에 AuraPlayerState의 SpellPoints 변경을 전달하는 델레게이트 추가 SpellPoints에 변경이 일어나면 해당 델레게이트를 실행하도록 Binding 초기 값을 전달할 때 SpellPoints 값을 전달하도록 설정

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -2,3 +2,22 @@
 
 
 #include "UI/WidgetController/SpellMenuWidgetController.h"
+
+#include "Player/AuraPlayerState.h"
+
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+	// SpellPoints 값 전달
+	const AAuraPlayerState* AuraPS = GetAuraPlayerStateChecked();
+	OnSpellPointsChangedDelegate.Broadcast(AuraPS->GetSpellPoints());
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+	// SpellPoints가 변경되면 그 값을 전달
+	AAuraPlayerState* AuraPS = GetAuraPlayerStateChecked();
+	AuraPS->OnSpellPointsChangedDelegate.AddWeakLambda(this, [this](int32 Value)
+	{
+		OnSpellPointsChangedDelegate.Broadcast(Value);
+	});
+}
--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -6,6 +6,9 @@
 #include "AuraWidgetController.h"
 #include "SpellMenuWidgetController.generated.h"
 
+// AuraPlayerState의 SpellPoints 값이 변경되면 그 값을 전달하는 델레게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSpellPointsChangedSignature, int32, Value);
+
 /**
  * Spell Menu에서 사용할 Widget Controller
  */
@@ -14,4 +17,10 @@ class AURA_API USpellMenuWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
 
+public:
+	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbacksToDependencies() override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnSpellPointsChangedSignature OnSpellPointsChangedDelegate;
 };
