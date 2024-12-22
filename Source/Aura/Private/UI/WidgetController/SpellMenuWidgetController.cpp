b23bcbce689cc73c589a760cba9da0f5d102c50c
hayoonleeMe <htdobad@gmail.com>
2024-12-22 15:14:36 +0900

    SpellMenuWidgetController에 Spell Globe를 선택하는 기능 추가

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -3,6 +3,7 @@
 
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 
+#include "Data/SpellConfig.h"
 #include "Player/AuraPlayerState.h"
 
 void USpellMenuWidgetController::BroadcastInitialValues()
@@ -20,4 +21,41 @@ void USpellMenuWidgetController::BindCallbacksToDependencies()
 	{
 		OnSpellPointsChangedDelegate.Broadcast(Value);
 	});
+}
+
+bool USpellMenuWidgetController::IsSelectedSpellOffensive() const
+{
+	return SelectedSpellTag.MatchesTag(FGameplayTag::RequestGameplayTag(TEXT("Abilities.Offensive")));
+}
+
+
+void USpellMenuWidgetController::SelectSpellGlobe(const FGameplayTag& SpellTag)
+{
+	if (SelectedSpellTag.MatchesTagExact(SpellTag))
+	{
+		// 선택했던 Spell Globe를 선택 해제하고 초기화
+		SelectedSpellTag = FGameplayTag::EmptyTag;
+		
+		// 기존 Spell Globe 선택 해제 및 Sound 재생
+		SelectSpellGlobeDelegate.Broadcast(false, SpellTag, true);
+	}
+	else
+	{
+		// 기존 Spell Globe 선택 해제
+		SelectSpellGlobeDelegate.Broadcast(false, SelectedSpellTag, false);
+		
+		// 새 Spell Globe 선택
+		SelectedSpellTag = SpellTag;
+		SelectSpellGlobeDelegate.Broadcast(true, SelectedSpellTag, false);
+	}
+}
+
+void USpellMenuWidgetController::DeselectSpellGlobe()
+{
+	if (SelectedSpellTag.IsValid())
+	{
+		const FGameplayTag DupSelectedSpellTag = SelectedSpellTag;
+		SelectedSpellTag = FGameplayTag::EmptyTag;
+		SelectSpellGlobeDelegate.Broadcast(false, DupSelectedSpellTag, false);
+	}
 }
--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -4,10 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AuraWidgetController.h"
+#include "GameplayTagContainer.h"
 #include "SpellMenuWidgetController.generated.h"
 
+class USpellConfig;
 // AuraPlayerState의 SpellPoints 값이 변경되면 그 값을 전달하는 델레게이트
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSpellPointsChangedSignature, int32, Value);
+
+// Spell Tag를 가지는 Spell Globe의 선택 상태 변경을 전달하는 델레게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FSelectSpellGlobeSignature, bool, bSelected, const FGameplayTag&, SpellTag, bool, bPlayDeselectSound);
 
 /**
  * Spell Menu에서 사용할 Widget Controller
@@ -23,4 +28,40 @@ public:
 
 	UPROPERTY(BlueprintAssignable)
 	FOnSpellPointsChangedSignature OnSpellPointsChangedDelegate;
+
+	UPROPERTY(BlueprintAssignable)
+	FSelectSpellGlobeSignature SelectSpellGlobeDelegate;
+
+
+	// 현재 선택한 Spell Globe가 있는지 반환
+	UFUNCTION(BlueprintCallable)
+	bool IsValidSelectedSpellTag() const { return SelectedSpellTag.IsValid(); }
+
+	// 현재 선택한 SpellGlobe의 Spell이 Offensive Spell인지를 반환
+	// true면 Offensive Spell, false면 Passive Spell
+	UFUNCTION(BlueprintCallable)
+	bool IsSelectedSpellOffensive() const;
+
+	
+	// 현재 Spell Menu에서 선택 중인 Spell Globe의 Spell Tag 캐싱
+	UPROPERTY(BlueprintReadWrite)
+	FGameplayTag SelectedSpellTag;
+
+	// Spell Menu에서 특정 Spell Globe를 선택
+	// SelectedSpellTag를 업데이트하고, SelectSpellGlobeDelegate 실행
+	UFUNCTION(BlueprintCallable)
+	void SelectSpellGlobe(const FGameplayTag& SpellTag);
+
+	// 현재 선택된 Spell Globe를 선택 해제한다.
+	UFUNCTION(BlueprintCallable)
+	void DeselectSpellGlobe();
+
+
+	// EquippedSpellRow Widget의 Globe를 선택하기를 기다리는 상태를 나타냄
+	UPROPERTY(BlueprintReadWrite)
+	uint8 bWaitSelectGlobe : 1;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	TObjectPtr<USpellConfig> SpellConfig;
 };
