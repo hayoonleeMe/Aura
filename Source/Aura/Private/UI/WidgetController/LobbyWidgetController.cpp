45cbf50e765e7ff142415eb6b52c617bdecf5058
hayoonleeMe <htdobad@gmail.com>
2025-01-23 23:17:54 +0900

    Lobby Overlay 위젯의 Exit 버튼 클릭 시 현재 세션을 제거하고 MainMenu로 돌아가는 기능 구현 Start 버튼 클릭 시 실제 Game Level로 Server Travel하는 기능 구현

--- Source/Aura/Private/UI/WidgetController/LobbyWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/LobbyWidgetController.cpp
@@ -19,4 +19,36 @@ void ULobbyWidgetController::BindCallbacksToDependencies()
 			MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
 		}
 	}
+
+	if (MultiplayerSessionsSubsystem)
+	{
+		// DestroySession이 종료되면 호출할 콜백 함수 등록
+		MultiplayerSessionsSubsystem->AuraOnDestroySessionCompleteDelegate.AddUObject(this, &ThisClass::OnDestroySessionComplete);
+	}
+}
+void ULobbyWidgetController::ReturnToMainMenu()
+{
+	if (MultiplayerSessionsSubsystem)
+	{
+		MultiplayerSessionsSubsystem->DestroySession();
+	}
+}
+
+void ULobbyWidgetController::StartGame(const APlayerController* OwningPlayerController)
+{
+	if (IsValid(OwningPlayerController) && OwningPlayerController->HasAuthority())
+	{
+		GetWorld()->ServerTravel(TEXT("/Game/Maps/StartupMap?listen"));
+	}
+}
+
+void ULobbyWidgetController::OnDestroySessionComplete(bool bWasSuccessful) const
+{
+	if (bWasSuccessful)
+	{
+		if (UGameInstance* GameInstance = GetWorld()->GetGameInstance())
+		{
+			GameInstance->ReturnToMainMenu();
+		}
+	}
 }
--- Source/Aura/Public/UI/WidgetController/LobbyWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/LobbyWidgetController.h
@@ -20,7 +20,19 @@ public:
 	virtual void BroadcastInitialValues() override;
 	virtual void BindCallbacksToDependencies() override;
 
+
+	// 로비를 나가 MainMenu로 이동
+	UFUNCTION(BlueprintCallable)
+	void ReturnToMainMenu();
+
+	// Server Travel to Game Level, server only
+	UFUNCTION(BlueprintCallable)
+	void StartGame(const APlayerController* OwningPlayerController);
+
 private:
 	UPROPERTY()
 	TObjectPtr<UMultiplayerSessionsSubsystem> MultiplayerSessionsSubsystem;
+
+	// Destroy Session 작업이 완료됐을 때의 콜백 함수
+	void OnDestroySessionComplete(bool bWasSuccessful) const;
 };
