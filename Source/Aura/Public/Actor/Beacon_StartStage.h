d7d9e700a8cdfbc8bffc280bc8d9054d91d43821
hayoonleeMe <htdobad@gmail.com>
2025-02-13 17:45:54 +0900

    Stage를 시작하는 기능을 수행할 Beacon Actor 생성 및 블루프린트 생성 Red를 제외한 다른 색으로 Highlight 할 수 있도록 PP_Highlight Material 변경

---
+++ Source/Aura/Public/Actor/Beacon_StartStage.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractionInterface.h"
+#include "Beacon_StartStage.generated.h"
+
+class UBoxComponent;
+
+/**
+ * 다음 스테이지를 시작하는 상호작용이 가능한 Beacon Actor
+ */
+UCLASS()
+class AURA_API ABeacon_StartStage : public AActor, public IInteractionInterface
+{
+	GENERATED_BODY()
+
+public:
+	ABeacon_StartStage();
+
+	/* Begin InteractionInterface */
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+	virtual void Interact() override;
+	/* End InteractionInterface */
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UBoxComponent> BoxComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+	UFUNCTION(Server, Reliable)
+	void ServerInteract();
+};
