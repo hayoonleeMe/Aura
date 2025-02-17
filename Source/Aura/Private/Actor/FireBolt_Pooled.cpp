7bb98329f327fe25b15dffcc8e8ad67dc2b77682
hayoonleeMe <htdobad@gmail.com>
2025-02-17 22:52:36 +0900

    BP_FireBolt_Pooled가 모든 기기에서 Overlap이 발생할 수 있도록 구현 그에 따라 서버에서만 데미지를 입힐 수 있도록 변경

--- Source/Aura/Private/Actor/FireBolt_Pooled.cpp
+++ Source/Aura/Private/Actor/FireBolt_Pooled.cpp
@@ -3,11 +3,14 @@
 
 #include "Actor/FireBolt_Pooled.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AuraBlueprintLibrary.h"
 #include "NiagaraComponent.h"
 #include "Component/PooledActorComponent.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "AbilitySystemComponent.h"
 
 AFireBolt_Pooled::AFireBolt_Pooled()
 {
@@ -42,4 +45,36 @@ void AFireBolt_Pooled::SetInUse(bool bInUse) const
 		LoopingSoundComponent->Deactivate();
 		FireEffectComponent->Deactivate();
 	}
+}
+
+void AFireBolt_Pooled::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsValidOverlap(OtherActor))
+	{
+		return;
+	}
+
+	// Can apply damage only in server
+	if (HasAuthority())
+	{
+		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
+		{
+			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;	
+			UAuraBlueprintLibrary::ApplyDamageEffect(DamageEffectParams);
+		}
+	}
+
+	if (ImpactCueTag.IsValid())
+	{
+		UAuraBlueprintLibrary::ExecuteGameplayCue(GetOwner(), ImpactCueTag, GetActorLocation());
+	}
+
+	PooledActorComponent->SetInUse(false);
+}
+
+bool AFireBolt_Pooled::IsValidOverlap(const AActor* TargetActor) const
+{
+	const AActor* SourceAvatarActor = DamageEffectParams.SourceAbilitySystemComponent ? DamageEffectParams.SourceAbilitySystemComponent->GetAvatarActor() : nullptr;
+	return IsValid(SourceAvatarActor) && IsValid(TargetActor) && SourceAvatarActor != TargetActor && UAuraBlueprintLibrary::IsNotFriend(SourceAvatarActor, TargetActor); 
 }
--- Source/Aura/Public/Actor/FireBolt_Pooled.h
+++ Source/Aura/Public/Actor/FireBolt_Pooled.h
@@ -22,6 +22,10 @@ public:
 	AFireBolt_Pooled();
 	virtual void PostInitializeComponents() override;
 
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual bool IsValidOverlap(const AActor* TargetActor) const override;
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UPooledActorComponent> PooledActorComponent;
