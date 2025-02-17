1efa79f90d7b12e95db7ffa3e2eca3e09d9df482
hayoonleeMe <htdobad@gmail.com>
2025-02-17 22:16:51 +0900

    Object Pool을 관리하는 Actor Component 구현 AuraCharacter에 UObjectPoolComponent 추가 Actor에 등록된 UObjectPoolComponent의 함수를 호출하는 인터페이스 생성 및 AAuraCharacter에서 구현

---
+++ Source/Aura/Public/Interface/ObjectPoolInterface.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ObjectPoolInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UObjectPoolInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Actor에 등록된 UObjectPoolComponent의 함수를 호출하는 인터페이스
+ */
+class AURA_API IObjectPoolInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Class 타입에 해당하는 Object Pool에서 사용할 수 있는 Actor를 가져와 SpawnTransform을 설정하고 반환한다.
+	virtual AActor* SpawnFromPool(const TSubclassOf<AActor>& Class, const FTransform& SpawnTransform) { return nullptr; }
+
+	template<class T>
+	T* SpawnFromPool(const TSubclassOf<AActor>& Class, const FTransform& SpawnTransform)
+	{
+		return Cast<T>(SpawnFromPool(Class, SpawnTransform));
+	}
+};
