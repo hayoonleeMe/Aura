67cacdb9cecf0bebf519c40f620818b66ff4da8c
hayoonleeMe <htdobad@gmail.com>
2025-03-02 00:45:05 +0900

    WBP_TextValueRow의 부모 클래스를 UTextValueRow로 설정

---
+++ Source/Aura/Public/UI/Widget/TextValueRow.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraUserWidget.h"
+#include "GameplayTagContainer.h"
+#include "TextValueRow.generated.h"
+
+class UToolTip_AttributeRow;
+struct FAttributeInfo;
+class UFramedValue;
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UTextValueRow : public UAuraUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> Text_Label;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> ToolTipWidgetClass;
+	
+	UFUNCTION()
+	UWidget* GetTextLabelTooltipWidget();
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UFramedValue> FramedValue;
+	
+	FText Description;
+	FGameplayTag AttributeTag;
+
+	void UpdateAttributeValueChange(const FAttributeInfo& AttributeInfo);
+};
