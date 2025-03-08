55ee4a7603e2b2fe48c8f76b38f133c935d71911
hayoonleeMe <htdobad@gmail.com>
2025-03-08 14:55:21 +0900

    FireBall Projectile이 폭발한 뒤 주변으로 발사되는 EmberBolt Projectile 클래스 생성

---
+++ Source/Aura/Private/Actor/EmberBolt.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/EmberBolt.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AEmberBolt::AEmberBolt()
+{
+	ProjectileMovementComponent->InitialSpeed = 700.f;
+	ProjectileMovementComponent->MaxSpeed = 700.f;
+}
