4d70ce628099823a6969203816569e1698d1696a
hayoonleeMe <htdobad@gmail.com>
2025-03-12 21:52:23 +0900

    SoundOptionMenu 옵션 설정 구현

--- Source/Aura/Private/UI/Widget/SoundOptionMenu.cpp
+++ Source/Aura/Private/UI/Widget/SoundOptionMenu.cpp
@@ -2,3 +2,57 @@
 
 
 #include "UI/Widget/SoundOptionMenu.h"
+
+#include "Components/Slider.h"
+#include "GameUserSettings/AuraGameUserSettings.h"
+#include "UI/Widget/OptionSliderRow.h"
+
+FOriginalSoundOptions::FOriginalSoundOptions(const UAuraGameUserSettings* AuraGameUserSettings)
+{
+	MasterVolume = AuraGameUserSettings->GetMasterVolumeValue();
+}
+
+void USoundOptionMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	bMasterVolumeChanged = false;
+	
+	AuraGameUserSettings = CastChecked<UAuraGameUserSettings>(GEngine ? GEngine->GetGameUserSettings() : nullptr);
+	OriginalSoundOptions = FOriginalSoundOptions(AuraGameUserSettings);
+
+	Row_MasterVolume->InitializeSliderValue(AuraGameUserSettings->GetMasterVolumeValue());
+	Row_MasterVolume->Slider->OnValueChanged.AddDynamic(this, &ThisClass::OnMasterVolumeOptionChanged);
+}
+
+void USoundOptionMenu::NativeDestruct()
+{
+	Super::NativeDestruct();
+
+	// 적용되지 않은 옵션을 원래대로 되돌림
+	if (bMasterVolumeChanged)
+	{
+		AuraGameUserSettings->SetMasterVolumeValue(OriginalSoundOptions.MasterVolume);
+	}
+}
+
+void USoundOptionMenu::OnOptionSaved()
+{
+	bMasterVolumeChanged = false;
+
+	// 저장했으므로 다시 업데이트
+	OriginalSoundOptions = FOriginalSoundOptions(AuraGameUserSettings);
+}
+
+bool USoundOptionMenu::HasOptionChanged() const
+{
+	return bMasterVolumeChanged;
+}
+
+void USoundOptionMenu::OnMasterVolumeOptionChanged(float Value)
+{
+	const int32 NewMasterVolume = int32(Value);
+	bMasterVolumeChanged = OriginalSoundOptions.MasterVolume != NewMasterVolume;
+	AuraGameUserSettings->SetMasterVolumeValue(NewMasterVolume);
+	OnOptionChangedDelegate.Broadcast();
+}
--- Source/Aura/Public/UI/Widget/SoundOptionMenu.h
+++ Source/Aura/Public/UI/Widget/SoundOptionMenu.h
@@ -6,7 +6,22 @@
 #include "Blueprint/UserWidget.h"
 #include "SoundOptionMenu.generated.h"
 
+class UAuraGameUserSettings;
 class UOptionSliderRow;
+
+// 해당 메뉴의 옵션이 변경됐음을 알리는 델레게이트
+DECLARE_MULTICAST_DELEGATE(FOnOptionChangedSignature);
+
+/**
+ * 변경되기 전 Sound Option을 저장할 구조체
+ */
+struct FOriginalSoundOptions
+{
+	int32 MasterVolume;
+
+	FOriginalSoundOptions() = default;
+	FOriginalSoundOptions(const UAuraGameUserSettings* AuraGameUserSettings);
+};
 
 /**
  * 
@@ -16,7 +31,31 @@ class AURA_API USoundOptionMenu : public UUserWidget
 {
 	GENERATED_BODY()
 
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	
 public:
+	// 변경사항이 적용되고 수행
+	void OnOptionSaved();
+
+	// 아직 저장되지 않은 변경된 옵션이 있는지 반환
+	bool HasOptionChanged() const;
+	
+	FOnOptionChangedSignature OnOptionChangedDelegate;
+	
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UOptionSliderRow> Row_MasterVolume;
+
+	UFUNCTION()
+	void OnMasterVolumeOptionChanged(float Value);
+
+private:
+	UPROPERTY()
+	TObjectPtr<UAuraGameUserSettings> AuraGameUserSettings;
+
+	FOriginalSoundOptions OriginalSoundOptions;
+
+	// 각 옵션이 변경됐는지 여부
+	uint8 bMasterVolumeChanged:1;
 };
