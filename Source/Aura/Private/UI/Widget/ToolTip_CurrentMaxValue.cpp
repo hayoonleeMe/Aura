96ead4199f0833cfd9d66fd6873c36923d941040
hayoonleeMe <htdobad@gmail.com>
2025-04-02 00:21:05 +0900

    Health, Mana Globe의 ToolTip Widget이 값을 표시할 때 소수점 한자리까지만 표시하도록 변경

--- Source/Aura/Private/UI/Widget/ToolTip_CurrentMaxValue.cpp
+++ Source/Aura/Private/UI/Widget/ToolTip_CurrentMaxValue.cpp
@@ -5,8 +5,15 @@
 
 #include "Components/TextBlock.h"
 
+UToolTip_CurrentMaxValue::UToolTip_CurrentMaxValue(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	NumberFormattingOptions.MinimumFractionalDigits = 1;
+	NumberFormattingOptions.MaximumFractionalDigits = 1;
+}
+
 void UToolTip_CurrentMaxValue::UpdateValues(float CurrentValue, float MaxValue) const
 {
-	TextBlock_CurrentValue->SetText(FText::AsNumber(CurrentValue));
-	TextBlock_MaxValue->SetText(FText::AsNumber(MaxValue));
+	TextBlock_CurrentValue->SetText(FText::AsNumber(CurrentValue, &NumberFormattingOptions));
+	TextBlock_MaxValue->SetText(FText::AsNumber(MaxValue, &NumberFormattingOptions));
 }
