12e64fe4628d24b6e3784b750cca07f8d914de15
hayoonleeMe <htdobad@gmail.com>
2025-04-07 15:54:37 +0900

    Level Sequence를 관리하는 Actor Component 클래스 생성

---
+++ Source/Aura/Private/Component/LevelSequenceManageComponent.cpp
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/LevelSequenceManageComponent.h"
+
+#include "AuraBlueprintLibrary.h"
+#include "LevelSequenceActor.h"
+#include "LevelSequencePlayer.h"
+
+ULevelSequenceManageComponent::ULevelSequenceManageComponent()
+{
+	bAutoActivate = true;
+	bWantsInitializeComponent = true;
+	SetIsReplicatedByDefault(false);
+}
+
+void ULevelSequenceManageComponent::SetLevelSequenceTags(TArray<FName>&& Tags)
+{
+	LevelSequenceActorTags = MoveTemp(Tags);
+}
+
+void ULevelSequenceManageComponent::AttachLevelSequenceActorToPawn(const FName& LevelSequenceTag, APawn* Pawn, bool bJumpToEndTime)
+{
+	if (!IsValid(Pawn) || !LevelSequenceActorMap.Contains(LevelSequenceTag))
+	{
+		return;
+	}
+
+	if (ALevelSequenceActor* LevelSequenceActor = LevelSequenceActorMap[LevelSequenceTag])
+	{
+		// Pawn에 부착한다.
+		if (LevelSequenceActor->AttachToActor(Pawn, FAttachmentTransformRules::SnapToTargetNotIncludingScale))
+		{
+			if (bJumpToEndTime)
+			{
+				if (ULevelSequencePlayer* LevelSequencePlayer = LevelSequenceActor->SequencePlayer)
+				{
+					// 중단됐다가 다시 부착된다면 EndTime에서 재생
+					if (LevelSequencePlayer->IsPaused())
+					{
+						const FMovieSceneSequencePlaybackParams Params(LevelSequencePlayer->GetEndTime().Time, EUpdatePositionMethod::Jump);
+						LevelSequencePlayer->SetPlaybackPosition(Params);
+						LevelSequencePlayer->Play();
+					}
+				}
+			}
+		}
+	}
+}
+
+void ULevelSequenceManageComponent::PlayLevelSequence(const FName& LevelSequenceTag)
+{
+	if (LevelSequenceActorMap.Contains(LevelSequenceTag))
+	{
+		if (const ALevelSequenceActor* LevelSequenceActor = LevelSequenceActorMap[LevelSequenceTag])
+		{
+			if (ULevelSequencePlayer* LevelSequencePlayer = LevelSequenceActor->SequencePlayer)
+			{
+				LevelSequencePlayer->Play();
+				CurrentPlayingLevelSequenceTag = LevelSequenceTag;
+			}
+		}
+	}
+}
+
+void ULevelSequenceManageComponent::StopLevelSequence(const FName& LevelSequenceTag)
+{
+	if (LevelSequenceActorMap.Contains(LevelSequenceTag))
+	{
+		if (const ALevelSequenceActor* LevelSequenceActor = LevelSequenceActorMap[LevelSequenceTag])
+		{
+			if (ULevelSequencePlayer* LevelSequencePlayer = LevelSequenceActor->SequencePlayer)
+			{
+				LevelSequencePlayer->Stop();
+			}
+		}
+	}
+}
+
+void ULevelSequenceManageComponent::SetLevelSequenceActorLocation(const FName& LevelSequenceTag, const FVector& NewLocation)
+{
+	if (LevelSequenceActorMap.Contains(LevelSequenceTag))
+	{
+		if (ALevelSequenceActor* LevelSequenceActor = LevelSequenceActorMap[LevelSequenceTag])
+		{
+			LevelSequenceActor->SetActorLocation(NewLocation);
+		}
+	}
+}
+
+void ULevelSequenceManageComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	// Level에 배치된 Level Sequence Actor 캐싱
+	for (const FName& Tag : LevelSequenceActorTags)
+	{
+		if (ALevelSequenceActor* LevelSequenceActor = UAuraBlueprintLibrary::GetLevelSequenceActorMatchingTag(GetWorld(), Tag))
+		{
+			if (ULevelSequencePlayer* LevelSequencePlayer = LevelSequenceActor->SequencePlayer)
+			{
+				LevelSequencePlayer->OnStop.AddDynamic(this, &ThisClass::OnLevelSequencePlayerStop);
+			}
+			LevelSequenceActorMap.Add(Tag, LevelSequenceActor);
+		}
+	}
+}
+
+void ULevelSequenceManageComponent::OnLevelSequencePlayerStop()
+{
+	OnLevelSequenceStopDelegate.Broadcast(CurrentPlayingLevelSequenceTag);
+}
