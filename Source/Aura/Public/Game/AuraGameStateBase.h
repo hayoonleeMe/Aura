c8ab8e6cb66824fe42c2ce3d102ebd713070eb0c
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-04-07 16:02:44 +0900

    level sequence system refactoring (#285)
    * 기존 PauseMenu Level Sequence 변경
    - CineCameraActor를 Spawnable로 변경
    - Instance Data - Transform Origin Actor를 LS_PauseMenu 자신으로 설정
    
    * Level Sequence를 관리하는 Actor Component 클래스 생성
    
    * AAuraPlayerController에 ULevelSequenceManageComponent 추가
    
    * IPlayerInterface에 ULevelSequenceManageComponent를 이용하는 가상 함수 추가
    
    * 기존 AAuraGameStateBase를 이용하는 대신 ULevelSequenceManageComponent를 사용하도록 변경

--- Source/Aura/Public/Game/AuraGameStateBase.h
+++ Source/Aura/Public/Game/AuraGameStateBase.h
@@ -8,7 +8,6 @@
 
 class UAttributeConfig;
 class USpellConfig;
-class ALevelSequenceActor;
 class UAuraInputConfig;
 class UInputMappingContext;
 
@@ -20,9 +19,6 @@ class AURA_API AAuraGameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
 
-protected:
-	virtual void PostInitializeComponents() override;
-	
 public:
 	AAuraGameStateBase();
 	
@@ -55,17 +51,4 @@ public:
 	// 리스폰에 걸리는 시간
 	UPROPERTY(EditDefaultsOnly, Category="Aura|Respawn")
 	float RespawnTime;
-
-	// ============================================================================
-	// Level Sequence
-	// ============================================================================
-
-	// PauseMenu를 열 때 재생할 Level Sequence Actor로, Level에 배치된 액터를 캐싱한다.
-	// 해당 액터에 설정된 Level Sequence의 Cine Camera는 해당 Level Sequence Actor에 부착되고 Enable Look at Tracking 옵션을 사용한다.
-	// 이를 통해 해당 Level Sequence Actor의 부모 액터인 플레이어 캐릭터를 중심으로 바라보며 Level Sequence가 재생된다.
-	UPROPERTY()
-	TObjectPtr<ALevelSequenceActor> PauseMenuLevelSequenceActor;
-
-	// Level에 배치된 PauseMenu Level Sequence Actor를 Pawn에 부착한다.
-	void AttachPauseMenuLevelSequenceActorToPawn(APawn* Pawn) const;
 };
