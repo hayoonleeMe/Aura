6e1ebb43eccc86cc9739067d5aab9ee180f78f6d
hayoonleeMe <htdobad@gmail.com>
2025-04-18 23:38:26 +0900

    TryInteract Ability를 실제로 Activate 하기 전에 커서로 선택한 Actor에 상호작용할 수 있는지 체크하는 기능 구현

--- Source/Aura/Public/AbilitySystem/Abilities/AuraAbility_TryInteract.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraAbility_TryInteract.h
@@ -19,6 +19,7 @@ public:
 	UAuraAbility_TryInteract();
 	
 protected:
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const override;
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 private:
