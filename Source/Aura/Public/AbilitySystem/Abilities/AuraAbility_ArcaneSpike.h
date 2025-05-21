9d5d998af8c2e50f43195bc5a56931da75d44178
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-05-21 22:19:16 +0900

    UAuraAbility_ArcaneSpike 클래스가 UAuraAbility_CastConfirmBase 클래스를 상속받도록 변경 - 처음 실행 시 범위를 표시하고 확인 키를 입력하면 Arcane Shard를 소환해 데미지를 입힘 - 커서 위치를 기준으로 Arcane Shard를 랜덤하게 소환하는 대신 정해진 거리만큼 떨어진 곳에 일정한 간격으로 소환하도록 변경 - Arcane Spike Spell 설정 변경 - Arcane Spike를 Startup Abilities로 설정

--- Source/Aura/Public/AbilitySystem/Abilities/AuraAbility_ArcaneSpike.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraAbility_ArcaneSpike.h
@@ -3,14 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AuraDamageAbility.h"
+#include "AuraAbility_CastConfirmBase.h"
 #include "AuraAbility_ArcaneSpike.generated.h"
 
 /**
  * 땅에서 Arcane Shard가 튀어나와 일정 범위 내에 Damage를 입히는 Arcane Spike Spell
  */
 UCLASS()
-class AURA_API UAuraAbility_ArcaneSpike : public UAuraDamageAbility
+class AURA_API UAuraAbility_ArcaneSpike : public UAuraAbility_CastConfirmBase
 {
 	GENERATED_BODY()
 
@@ -21,25 +21,25 @@ public:
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	
-	// UAbilityTask_TargetDataUnderMouse::TargetDataUnderMouseSetDelegate 델레게이트의 Callback 함수
-	void OnTargetDataUnderMouseSet(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
-
+	virtual void OnTargetDataUnderMouseSet(const FGameplayAbilityTargetDataHandle& TargetDataHandle) override;
 	virtual void OnEventReceived(FGameplayEventData Payload) override;
+
+	virtual float GetRangeRadius() const override;
 
 private:
 	FVector CachedTargetLocation;
 
 	void SpawnArcaneShard() const;
 
-	// ECC_OnlyWall Trace Channel 사용해 ArcaneShard가 벽을 넘어가지 않을 유효한 위치를 계산해 반환
-	FVector GetAdjustedTargetLocation(const FVector& StartLocation, const FVector& TargetLocation, const FCollisionQueryParams& QueryParams, bool bDoLineTrace) const;
-
 	// 하나의 Arcane Shard의 유효 범위
-	static constexpr float EffectiveRadius = 100.f;
+	static constexpr float ShardEffectiveRadius = 100.f;
 	
-	// Spell의 최대 시전 사거리
-	UPROPERTY(EditDefaultsOnly, Category="Aura|ArcaneSpike")
-	float MaxCastRange;
+	// ShardEffectiveRadius * ScaleRate
+	float FinalShardEffectiveRadius = 0.f;
+
+	// 커서 위치에서 FinalShardEffectiveRadius만큼 떨어진 곳에 Arcane Shard를 소환하는데, 각 Shard의 중심은 동일한 간격으로 배치된다. (ex. Shard가 3개면 120도씩)
+	// 따라서 각 Shard를 모두 포함하는 최소 원의 반지름인 TotalEffectiveRadius = FinalShardEffectiveRadius * 2
+	float TotalEffectiveRadius = 0.f;
 
 	// EffectiveRadius에 곱해져 크기를 결정하는 ScaleRate를 저장하는 Curve 
 	UPROPERTY(EditDefaultsOnly, Category="Aura|ArcaneSpike")
